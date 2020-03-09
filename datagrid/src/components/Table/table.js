837d421b3845c4a6021c5a4bbf0d56b38d1bb2e1
Victoria-Butrym <victoria-butrym@tut.by>
2020-03-09 19:56:21 +0300

    feat: make № column sticky

--- datagrid/src/components/Table/table.js
+++ datagrid/src/components/Table/table.js
@@ -2,15 +2,15 @@ import React from "react";
 
 const Table = ({ data }) => {
   return (
-    <>
+    <div className="table-wrapper">
       <table className="table">
         <thead className="table-head">
           <tr>
-            <th>№</th>
+            <th className="center sticky position-header">№</th>
             <th>Name</th>
-            <th>Employed</th>
+            <th className="center">Employed</th>
             <th>Speciality</th>
-            <th>Score</th>
+            <th className="center">Score</th>
             <th>BIC</th>
             <th>Email</th>
           </tr>
@@ -18,8 +18,8 @@ const Table = ({ data }) => {
         <tbody>
           {data.map(user => (
             <tr key={user.id} className="table-row">
-              <td className="center">{user.position}</td>
-              <td>{user.name}</td>
+              <td className="center position sticky">{user.position}</td>
+              <td className="name">{user.name}</td>
               <td className="center">{user.boolean}</td>
               <td>{user.jobTitle}</td>
               <td className="center">{user.score}</td>
@@ -28,26 +28,8 @@ const Table = ({ data }) => {
             </tr>
           ))}
         </tbody>
-        {/* <span
-          className="table-head"
-          style={{ color: "#5b0b0d", fontWeight: "600" }}
-        >
-          № Name Employed Speciality IP BIC
-        </span> */}
-        {/* <ul className="rows">
-          {data.map(user => (
-            <li key={user.id} className="row-item">
-              <span style={{ margin: "0 1rem" }}>{user.position}</span>
-              <span style={{ margin: "0 1rem" }}>{user.name}</span>
-              <span style={{ margin: "0 1rem" }}>{user.boolean}</span>
-              <span style={{ margin: "0 1rem" }}>{user.jobTitle}</span>
-              <span style={{ margin: "0 1rem" }}>{user.ip}</span>
-              <span style={{ margin: "0 1rem" }}>{user.finance}</span>
-            </li>
-          ))}
-        </ul> */}
       </table>
-    </>
+    </div>
   );
 };
 
